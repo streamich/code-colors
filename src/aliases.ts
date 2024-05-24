936d3741a453d88cfaf283b0403547fcb35aebe8
streamich <streamich@gmail.com>
2024-05-24 15:55:00 +0200

    feat: 🎸 add language aliases

---
+++ src/aliases.ts
@@ -0,0 +1,82 @@
+export const aliases: Record<string, string> = {
+  feature: 'cucumber',
+
+  adb: 'ada',
+  ads: 'ada',
+
+  ahkl: 'ahk',
+
+  htaccess: 'apacheconf',
+  htgroups: 'apacheconf',
+  'apache.conf': 'apacheconf',
+  'apache2.conf': 'apacheconf',
+
+  as3: 'as',
+
+  sh: 'bash',
+  ksh: 'bash',
+  ebuild: 'bash',
+  eclass: 'bash',
+
+  cmd: 'bat',
+
+  bf: 'brainfuck',
+  b: 'brainfuck',
+
+  h: 'c',
+
+  cfml: 'cfm',
+  cfc: 'cfm',
+
+  js: 'javascript',
+  ecmascript: 'javascript',
+  cjs: 'javascript',
+  mjs: 'javascript',
+
+  coffee: 'coffeescript',
+
+  ts: 'typescript',
+  'd.ts': 'typescript',
+
+  list: 'cl',
+  el: 'cl',
+
+  clj: 'clojure',
+  cljs: 'clojure',
+  
+  hpp: 'cpp',
+  'c++': 'cpp',
+  'h++': 'cpp',
+  cc: 'cpp',
+  hh: 'cpp',
+  cxx: 'cpp',
+  hxx: 'cpp',
+  pde: 'cpp',
+
+  cs: 'csharp',
+
+  py: 'python',
+  pyx: 'python',
+  pxd: 'python',
+  pxi: 'python',
+  pyw: 'python',
+  sc: 'python',
+  sconstruct: 'python',
+  tac: 'python',
+  sconscript: 'python',
+
+  di: 'd',
+
+  mak: 'makefile',
+  gnumakefile: 'makefile',
+
+  md: 'markdown',
+
+  xsl: 'xml',
+  rss: 'xml',
+  xslt: 'xml',
+  xsd: 'xml',
+  wsdl: 'xml',
+
+  yml: 'yaml',
+};
